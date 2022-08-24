6067e70fc37f1240e5829e445a55641c171d370f
OlAndrey <oleynik.andrey01@gmail.com>
2022-08-24 15:08:21 +0300

    chore(Intro): created and styled Intro component

---
+++ src/Componets/Intro.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Intro = () => {
+    return (
+        <section className="intro">
+            <div className="container">
+                <div className="intro__wrapper">
+                    <h1 className="intro__title">Hello, I am Andrey Oleynik</h1>
+                    <p className="intro__subtitle">
+                        <strong>
+                        <Typed
+                            strings={[
+                            "Front End Developer",
+                            "Software Engineer"
+                            ]}
+                            typeSpeed={80}
+                            backDelay={1100}
+                            backSpeed={30}
+                            loop
+                        />
+                        </strong>
+                    </p>
+                    <p>
+                        <a
+                        className="button intro__button"
+                        href="#work"
+                        role="button"
+                        >
+                        View My Work
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Intro;
